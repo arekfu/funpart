c79bf06d83724db8462f041a79ae9dc79e549e35
Davide Mancusi <davide.mancusi@cea.fr>
2016-07-08 18:24:35 +0200

    Provide constructor function to hide Vec implementation

--- src/Vec.hs
+++ src/Vec.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 
 module Vec
-( Vec3
+( cart
 ) where
 
 import VecSpace
@@ -11,8 +11,6 @@ data Vec3 a = Vec3 a a a deriving Eq
 instance Show a => Show (Vec3 a) where
     show (Vec3 x y z) = "(" ++ show x ++ ", " ++ show y ++ ", " ++ show z ++ ")"
 
-type DVec3 = Vec3 Double
-
 instance Num a => VecSpace (Vec3 a) a where
     (Vec3 x0 y0 z0) +: (Vec3 x1 y1 z1) = Vec3 (x0+x1) (y0+y1) (z0+z1)
     s *: (Vec3 x y z) = Vec3 (s*x) (s*y) (s*z)
@@ -20,3 +18,6 @@ instance Num a => VecSpace (Vec3 a) a where
 
 instance Num a => EuclidVecSpace (Vec3 a) a where
     (Vec3 x0 y0 z0) .: (Vec3 x1 y1 z1) = x0*x1 + y0*y1 + z0*z1
+
+cart :: a -> a -> a -> Vec3 a
+cart = Vec3
