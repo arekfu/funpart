023581ac7dc6b45d6df489ce51ffa305c11cc9eb
Davide Mancusi <davide.mancusi@cea.fr>
2016-08-23 07:53:04 +0200

    Add deriving clauses and record names for Track datatypes

--- src/Track.hs
+++ src/Track.hs
@@ -2,6 +2,8 @@
 
 module Track
 ( TrackPointType(..)
+, collisionXSec
+, secondaries
 , TrackPoint(..)
 , pointType
 , pointVertex
@@ -16,17 +18,19 @@ import Control.Lens (makeLenses)
 import Particle
 import CrossSection
 
-data TrackPointType = CollisionPoint CrossSection [Track]
+data TrackPointType = CollisionPoint { _collisionXSec :: CrossSection, _secondaries :: [Track] }
                     | SourcePoint
                     | EndPoint
+                    deriving (Show, Eq)
 
 data TrackPoint = TrackPoint { _pointType     :: TrackPointType
                              , _pointVertex   :: Position
                              , _pointMomentum :: Momentum
                              , _pointWeight   :: Weight
-                             }
+                             } deriving (Show, Eq)
 
-newtype Track = Track { _trackPoints :: [TrackPoint] }
+newtype Track = Track { _trackPoints :: [TrackPoint] } deriving (Show, Eq)
 
+makeLenses ''TrackPointType
 makeLenses ''TrackPoint
 makeLenses ''Track
