2120d5e4d26ffd361b5c2572cd9e82a3e1b7d919
Davide Mancusi <davide.mancusi@cea.fr>
2016-08-23 08:06:30 +0200

    Define CrossSection typeclass

--- src/CrossSection.hs
+++ src/CrossSection.hs
@@ -1,7 +1,32 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module CrossSection
-( CrossSection
+( CrossSectionValue
+, CrossSection(..)
+, totXSConst
+, absXSConst
 ) where
 
-import Core
+import Control.Lens (makeLenses)
 
-type CrossSection = FPFloat
+import Core
+import Particle
+
+type CrossSectionValue = FPFloat
+
+class CrossSection a where
+    getAbsXS :: a -> Particle -> CrossSectionValue
+    getAbsXS xs p = max 0.0 $ getTotXS xs p - getScatXS xs p
+    getTotXS :: a -> Particle -> CrossSectionValue
+    getTotXS xs p = max 0.0 $ getAbsXS xs p + getScatXS xs p
+    getScatXS :: a -> Particle -> CrossSectionValue
+    getScatXS xs p = max 0.0 $ getTotXS xs p - getAbsXS xs p
+
+data ConstantXS = ConstantXS { _totXSConst :: CrossSectionValue
+                             , _absXSConst :: CrossSectionValue
+                             } deriving (Show, Eq)
+makeLenses ''ConstantXS
+
+instance CrossSection ConstantXS where
+    getAbsXS (ConstantXS _ absXS) _ = absXS
+    getTotXS (ConstantXS totXS _) _ = totXS
