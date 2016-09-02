3baae2e997bcb343d00796dcee88257f7f204994
Davide Mancusi <davide.mancusi@cea.fr>
2016-09-02 22:20:32 +0200

    Add Main program

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+import Control.Monad
+
+import SimSetup
+import Problem
+import CrossSection
+import Particle
+import Source
+import Source.Distributions
+import Track
+import Vec
+
+setup :: SimSetup
+setup = SimSetup { theXSec = xSec
+                 , initialSeed = seed
+                 , nShots = shots
+                 , source = aSource
+                 , scores = []
+                 }
+        where xSec = CrossSection $ ConstantXS totXSec absXSec
+              totXSec = 1.0
+              absXSec = 0.1
+              seed = 123456
+              shots = 3
+              aSource = Source $ FactorizedSource Neutron (PointwiseSpaceDistribution (Pos zero)) (IsoMonoDistribution 1)
+
 main :: IO ()
-main = return ()
+main = forM_ tracks $ \track -> print $ length $ _trackPoints track
+    where tracks = snd $ runProblem fixedSourceProblem setup
